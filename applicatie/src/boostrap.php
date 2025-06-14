6a858d82b66b04b9591daeb93ae4273b7ab87ede
ajmschaper <ajm.schaper@student.han.nl>
2025-06-14 23:53:16 +0200

    Added: config.php to be able to modify settings inside one file. Changed: boostrap.php to handle dynamic config.php settings.

--- applicatie/src/boostrap.php
+++ applicatie/src/boostrap.php
@@ -1,7 +1,17 @@
 <?php
 
-// Adds Session Handling Functionality
-require_once __DIR__ . '/../src/handlers/session-handler.php';
-
 // Adds Path Handling Functionality
 require_once __DIR__ . '/../src/handlers/path-handler.php';
+
+// Adds Session Handling Functionality
+require_once BASE_DIR . '/src/handlers/session-handler.php';
+
+// Adds View Functions
+require_once BASE_DIR . '/src/view/view-functions.php';
+
+// Sets up Config
+$config = require_once 'config.php';
+
+define('WEBSITE_TITLE', $config['websiteTitle']);
+define('CHARSET', $config['websiteCharset']);
+define('WEBSITE_LANGUAGE', $config['websiteLanguage']);
---
+++ applicatie/src/config.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'websiteTitle' => 'Pizzeria Sole Machina', // Sets Website Title (Name)
+    'websiteCharset' => 'UTF-8', // Sets Charset to UTF-8
+    'websiteLanguage' => 'en-US', // Sets Language
+];
