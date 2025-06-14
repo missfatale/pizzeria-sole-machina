873d261817aaee1b975c22a6f484c59e97751c63
ajmschaper <ajm.schaper@student.han.nl>
2025-06-14 23:52:19 +0200

    Added frontend Endpoints for Form Actions

---
+++ applicatie/public/actions/login-action.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * login-action.php
+ *
+ * Public-Facing Entrypoint that handles the login form submission.
+ * This file is only responsible for receiving POST data and routing it securely to interal logic.
+ */
+
+require_once __DIR__ . '/../../src/handlers/input-handlers/login-input-handler.php';
---
+++ applicatie/public/actions/register-action.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * register-action.php
+ *
+ * Public-Facing Entrypoint that handles the register form submission.
+ * This file is only responsible for receiving POST data and routing it securely to interal logic.
+ */
+
+require_once __DIR__ . '/../../src/handlers/input-handlers/register-input-handler.php';
