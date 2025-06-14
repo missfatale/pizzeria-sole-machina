bd437a09e99a84fa65b7c61d121e568e0b545efc
ajmschaper <ajm.schaper@student.han.nl>
2025-06-14 23:57:28 +0200

    Added: renderDefaultHeadSection() to be able to change Default <Head> Section in one place. getPageTitle() to handle pages not having a pageTitle variable setup.

--- applicatie/src/view/view-functions.php
+++ applicatie/src/view/view-functions.php
@@ -1,5 +1,27 @@
 <?php
 
+function getPageTitle(): string {
+    $websiteTitle = WEBSITE_TITLE;
+    $websitePage = $GLOBALS['pageTitle'] ?? null;
+
+    return $websitePage ? "$websitePage | $websiteTitle" : $websiteTitle;
+}
+
+function renderDefaultHeadSection(): void {
+    ?>
+    <meta charset="<?= htmlspecialchars(CHARSET) ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= htmlspecialchars(getPageTitle()) ?></title>
+    <link rel="icon" type="image/x-icon" href="<?= BASE_URL; ?>assets/favicon.ico">
+    <link rel="stylesheet" href="<?= BASE_URL; ?>assets/css/style.css">
+    <link rel="stylesheet" href="<?= BASE_URL; ?>assets/css/header.css">
+    <link rel="stylesheet" href="<?= BASE_URL; ?>assets/css/footer.css">
+    <link rel="stylesheet" href="<?= BASE_URL; ?>assets/css/all.min.css">
+    <link rel="stylesheet" href="<?= BASE_URL; ?>assets/css/fontawesome.min.css">
+    <?php
+}
+
 function viewLoginForm() {
     include __DIR__ . '/../../templates/forms/login-form.php';
 }
