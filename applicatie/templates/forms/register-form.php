3dd3241430ba8a1316af769a277d6ce1772262a8
ajmschaper <ajm.schaper@student.han.nl>
2025-06-14 21:25:22 +0200

    Updated register-form.php to use register inside naming.

--- applicatie/templates/forms/register-form.php
+++ applicatie/templates/forms/register-form.php
@@ -9,48 +9,48 @@
 </div>
 
 <div>
-    <label for="first-name">Voornaam:</label>
-    <input type="text" id="first-name" name="first_name" pattern="[A-Za-z\s]+" maxlength="15" required>
+    <label for="register-first-name">Voornaam:</label>
+    <input type="text" id="register-first-name" name="register-first_name" pattern="[A-Za-z\s]+" maxlength="15" required>
 </div>
 
 <div>
-    <label for="last-name">Achternaam:</label>
-    <input type="text" id="last-name" name="last_name" pattern="[A-Za-z\s]+" maxlength="15" required>
+    <label for="register-last-name">Achternaam:</label>
+    <input type="text" id="register-last-name" name="register-last_name" pattern="[A-Za-z\s]+" maxlength="15" required>
 </div>
 
 <div>
-    <label for="geboortedatum">Geboortedatum:</label>
-    <input type="date" id="geboortedatum" name="geboortedatum" required>
+    <label for="register-geboortedatum">Geboortedatum:</label>
+    <input type="date" id="register-geboortedatum" name="register-geboortedatum" required>
 </div>
 
 <div>
-    <label for="username">Gebruikersnaam:</label>
-    <input type="text" id="username" name="username" minlength="3" required>
+    <label for="register-username">Gebruikersnaam:</label>
+    <input type="text" id="register-username" name="register-username" minlength="3" required>
 </div>
 
 <div>
-    <label for="password">Wachtwoord:</label>
-    <input type="password" id="password" name="password" minlength="8" required>
+    <label for="register-password">Wachtwoord:</label>
+    <input type="password" id="register-password" name="register-password" minlength="8" required>
 </div>
 
 <div>
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" minlength="5" placeholder="gebruiker@provider.nl" required>
+    <label for="register-email">Email:</label>
+    <input type="email" id="register-email" name="register-email" minlength="5" placeholder="gebruiker@provider.nl" required>
 </div>
 
 <div>
-    <label for="mobile">Mobiele Nummer:</label>
-    <input type="text" id="mobile" name="mobile" placeholder="06-00000000" pattern="06-\d{8}" title="Must be in the format 06-XXXXXXXX" required>
+    <label for="register-mobile">Mobiele Nummer:</label>
+    <input type="text" id="register-mobile" name="register-mobile" placeholder="06-00000000" pattern="06-\d{8}" title="Must be in the format 06-XXXXXXXX" required>
 </div>
 
 <div>
-    <label for="postcode">Postcode:</label>
-    <input type="text" id="postcode" name="postcode" placeholder="1111 AB" pattern="\d{4}\s[A-Z]{2}" required>
+    <label for="register-postcode">Postcode:</label>
+    <input type="text" id="register-postcode" name="register-postcode" placeholder="1111 AB" pattern="\d{4}\s[A-Z]{2}" required>
 </div>
 
 <div>
-    <label for="address">Adres:</label>
-    <input type="text" id="address" name="address" minlength="5" required>
+    <label for="register-address">Adres:</label>
+    <input type="text" id="register-address" name="register-address" minlength="5" required>
 </div>
 
 <button type="submit" class="btn-secondary">Registreren</button>
