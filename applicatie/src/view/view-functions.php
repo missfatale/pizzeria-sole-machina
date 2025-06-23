5f5ddcf12767b068449263e271a93fe0475a5570
ajmschaper <ajm.schaper@student.han.nl>
2025-06-23 15:45:41 +0200

    Changed: Stylesheet Links

--- applicatie/src/view/view-functions.php
+++ applicatie/src/view/view-functions.php
@@ -13,12 +13,12 @@ function renderDefaultHeadSection(): void {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars(getPageTitle()) ?></title>
-    <link rel="icon" type="image/x-icon" href="<?= BASE_URL; ?>assets/favicon.ico">
-    <link rel="stylesheet" href="<?= BASE_URL; ?>assets/css/style.css">
-    <link rel="stylesheet" href="<?= BASE_URL; ?>assets/css/header.css">
-    <link rel="stylesheet" href="<?= BASE_URL; ?>assets/css/footer.css">
-    <link rel="stylesheet" href="<?= BASE_URL; ?>assets/css/all.min.css">
-    <link rel="stylesheet" href="<?= BASE_URL; ?>assets/css/fontawesome.min.css">
+    <link rel="icon" type="image/x-icon" href="<?= BASE_URL . '/assets/favicon.ico'?>">
+    <link rel="stylesheet" href="<?= BASE_URL . '/assets/css/style.css'?>">
+    <link rel="stylesheet" href="<?= BASE_URL . '/assets/css/header.css'?>">
+    <link rel="stylesheet" href="<?= BASE_URL . '/assets/css/footer.css'?>">
+    <link rel="stylesheet" href="<?= BASE_URL . '/assets/css/all.min.css'?>">
+    <link rel="stylesheet" href="<?= BASE_URL . '/assets/css/fontawesome.min.css'?>">
     <?php
 }
 
