b7896a0e6bb8d92aebd22f705abd90de6219e55c
ajmschaper <ajm.schaper@student.han.nl>
2025-06-23 16:29:49 +0200

    Added: routing-helper.php which checks whitelists and resolves category.php and page.php content.

--- applicatie/src/helpers/routing-helper.php
+++ applicatie/src/helpers/routing-helper.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * routing-helper.php
+ *
+ * Contains Helper Functions to Resolve Dynamic Routes to Specific Content Files.
+ * Uses Whitelists for Security to Prevent Arbitrary File Inclusion.
+ */
+
+// Resolves a Whitelisted Page Name to a File Path in /templates/pages
 function resolvePageContentFile($page, $whitelist, $defaultFile) {
     if (in_array($page, $whitelist)) {
         return BASE_DIR . "/templates/pages/{$page}.php";
@@ -8,10 +16,24 @@ function resolvePageContentFile($page, $whitelist, $defaultFile) {
     return $defaultFile;
 }
 
-function resolveCategoryContentFile() {
-
+// Resolves a Whitelisted Category Name to a File Path in /templates/categories
+function resolveCategoryContentFile($category, $whitelist, $defaultFile) {
+    foreach ($whitelist as $folder => $allowed) {
+        if (in_array($category, $allowed)) {
+            return BASE_DIR . "/templates/categories/{$category}.php";
+        }
+    }
+    http_response_code(404);
+    return $defaultFile;
 }
 
-function resolveItemContentFile() {
-
+// Resolves a Whitelisted Item Name to a File Path in /templates/items
+/* CONSIDERATION
+function resolveItemContentFile($item, $whitelist, $defaultFile) {
+    if (in_array($item, $whitelist)) {
+        return BASE_DIR . "/templates/items/{$item}.php";
+    }
+    http_response_code(404);
+    return $defaultFile;
 }
+*/
