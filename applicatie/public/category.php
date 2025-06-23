40ae18f7d9c0fbbda1f915b7db780a344af2e4f9
ajmschaper <ajm.schaper@student.han.nl>
2025-06-23 16:26:32 +0200

    Changed: Setup for default category.php

--- applicatie/public/category.php
+++ applicatie/public/category.php
@@ -3,4 +3,33 @@
 // Bootstrap: Loads all Core Configurations and Path Handlers for the Project.
 require_once __DIR__ . '/../src/boostrap.php';
 
+// Routing Logic
+require_once BASE_DIR . '/src/routing.php';
+
+// Load Whitelist
+$allowedCategories = require BASE_DIR . '/src/config/whitelist/category-whitelist.php';
+
+// Determine Which Category to Show
+$content = resolveCategoryContentFile($_GET['category'] ?? '', $allowedCategories, BASE_DIR . '/public/404.php');
+
 ?>
+
+<!DOCTYPE html>
+<html lang="<?= htmlspecialchars(WEBSITE_LANGUAGE) ?>">
+
+<head>
+    <?php renderDefaultHeadSection(); ?>
+</head>
+
+<body>
+
+<?php require_once BASE_DIR . '/templates/elements/header.php'; ?>
+
+<main>
+    <?php require $content; ?>
+</main>
+
+<?php require_once BASE_DIR . '/templates/elements/footer.php'; ?>
+
+</body>
+</html>
