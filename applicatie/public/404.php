85982afd5bc640b9eb6cafad0f3827312e19ef25
ajmschaper <ajm.schaper@student.han.nl>
2025-06-23 15:41:45 +0200

    Added: 404.css Changed: Build out foundational 404.php page

--- applicatie/public/404.php
+++ applicatie/public/404.php
@@ -1,6 +1,35 @@
 <?php
-
 // Bootstrap: Loads all Core Configurations and Path Handlers for the Project.
 require_once __DIR__ . '/../src/boostrap.php';
 
+// Page Title
+$pageTitle = "404";
 ?>
+
+<!DOCTYPE html>
+<html lang="<?= htmlspecialchars(WEBSITE_LANGUAGE) ?>">
+
+<head>
+    <?php renderDefaultHeadSection(); ?>
+    <link rel="stylesheet" href="<?= BASE_URL . '/assets/css/404.css' ?>">
+</head>
+
+<body>
+<?php require_once BASE_DIR . '/templates/elements/header.php'; ?>
+
+<main id="main-404">
+    <section class="not-found-box">
+        <div class="not-found-content bg-white">
+            <div class="not-found-text bg-white">
+                <h1>404</h1>
+                <p>Deze pagina bestaat niet of is verplaatst.</p>
+                <a class="bg-white" href="<?= INDEX_PAGE ?>"><button class="btn-secondary not-found-button">Terug naar Home</button></a>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php require_once BASE_DIR . '/templates/elements/footer.php'; ?>
+</body>
+
+</html>
