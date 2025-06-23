5bce65093b3a002f340a00a97e6e6f79cb33d422
ajmschaper <ajm.schaper@student.han.nl>
2025-06-23 15:47:12 +0200

    Changed: Robusted login-input-handler.php

--- applicatie/src/handlers/input-handlers/login-input-handler.php
+++ applicatie/src/handlers/input-handlers/login-input-handler.php
@@ -1,22 +1,39 @@
 <?php
 
-// Adds Path Handling Functionality
-require_once __DIR__ . '/../path-handler.php';
+// Bootstrap: Loads all Core Configurations and Path Handlers for the Project.
+require_once __DIR__ . '/../../boostrap.php';
 
-$LOGIN_PAGE = BASE_URL . '/../login.php';
-$INDEX_PAGE = BASE_URL . '/../index.php';
+require_once BASE_DIR . '/src/data/data-users.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = trim($_POST["login-username"]);
     $password = $_POST["login-password"];
 
+    // Username or Password Empty
     if (empty($username) || empty($password)) {
-        header("Location: $LOGIN_PAGE");
+        $_SESSION["login_error"] = "Gebruikersnaam en wachtwoord zijn verplicht.";
+        header('Location: ' . LOGIN_PAGE);
+        exit();
+    }
+
+    $user = loginUser($username, $password);
+
+    // Password Incorrect
+    if (!$user) {
+        $_SESSION["login_error"] = "Gebruikersnaam of wachtwoord is incorrect.";
+        header('Location: ' . LOGIN_PAGE);
+        exit();
+    }
+
+    // Logged In
+    else {
+        authenticateUser($user["username"], $user["role"], $user["first_name"]);
+        $_SESSION["is_logged_in"] = true;
+
+        header('Location: ' . INDEX_PAGE);
         exit();
     }
 }
 
-else {
-    header("Location: $LOGIN_PAGE");
-    exit();
-}
+header('Location: ' . INDEX_PAGE);
+exit();
