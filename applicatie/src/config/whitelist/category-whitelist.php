4f9a0fdb56adf0be113318a0ac7d7f7c27f47089
ajmschaper <ajm.schaper@student.han.nl>
2025-06-25 16:49:50 +0200

    Added: Documentated Files

--- applicatie/src/config/whitelist/category-whitelist.php
+++ applicatie/src/config/whitelist/category-whitelist.php
@@ -1,8 +1,19 @@
 <?php
 
-// Whitelisted Categories
+/**
+ * category-whitelist.php
+ *
+ * Bevat de whitelist van toegestane productcategorieën voor de applicatie.
+ *
+ * Array keys representeren hoofdgroepen (bijv. 'producten').
+ * Array values zijn lijsten van toegestane subcategorieën binnen die groepen.
+ *
+ * Voorbeeld gebruik:
+ * - Validatie van categorieën in URL parameters.
+ * - Filteren van getoonde items.
+ *
+ */
 
 return [
-    'aanbiedingen' => ['acties'],
-    'producten' => ['pizza', 'focaccia', 'dranken']
+    'producten' => ['voorgerecht', 'maaltijd', 'pizza', 'drank']
 ];
--- applicatie/src/config/whitelist/page-whitelist.php
+++ applicatie/src/config/whitelist/page-whitelist.php
@@ -1,5 +1,13 @@
 <?php
 
-// Whitelisted Pages
+/**
+ * page-whitelist.php
+ *
+ * Bevat een whitelist van toegestane pagina's binnen de applicatie.
+ *
+ * Wordt gebruikt om te controleren of een opgegeven pagina
+ * bijvoorbeeld via een queryparameter toegestaan is.
+ *
+ */
 
 return ['privacy'];
