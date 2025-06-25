aa56388e7c4a6e932c6e61ae43615b3177504704
ajmschaper <ajm.schaper@student.han.nl>
2025-06-25 16:08:04 +0200

    Changed: Refactored Code to be Consistent

--- applicatie/public/404.php
+++ applicatie/public/404.php
@@ -1,35 +1,39 @@
 <?php
-// Bootstrap: Loads all Core Configurations and Path Handlers for the Project.
-require_once __DIR__ . '/../src/boostrap.php';
 
-// Page Title
-$pageTitle = "404";
+// Bootstrap: Loads all Core Configurations and Path Handlers for the Project.
+require_once __DIR__ . '/../src/bootstrap.php';
+
 ?>
 
 <!DOCTYPE html>
 <html lang="<?= htmlspecialchars(WEBSITE_LANGUAGE) ?>">
 
 <head>
-    <?php renderDefaultHeadSection(); ?>
-    <link rel="stylesheet" href="<?= BASE_URL . '/assets/css/404.css' ?>">
+    <?php renderDefaultHeadSection() ?>
+    <link rel="stylesheet" href="<?= BASE_URL . '/assets/css/404.css'; ?>">
 </head>
 
 <body>
-<?php require_once BASE_DIR . '/templates/elements/header.php'; ?>
 
-<main id="main-404">
-    <section class="not-found-box">
-        <div class="not-found-content bg-white">
-            <div class="not-found-text bg-white">
-                <h1>404</h1>
-                <p>Deze pagina bestaat niet of is verplaatst.</p>
-                <a class="bg-white" href="<?= INDEX_PAGE ?>"><button class="btn-secondary not-found-button">Terug naar Home</button></a>
+    <!-- Header + Navigation -->
+    <?php require_once TEMPLATES_DIR . '/elements/header.php'; ?>
+
+    <!-- Main Content -->
+    <main id="main-404">
+        <section class="not-found-box">
+            <div class="not-found-content bg-white">
+                <div class="not-found-text bg-white">
+                    <h1>404</h1>
+                    <p>Deze pagina bestaat niet of is verplaatst.</p>
+                    <a class="bg-white" href="<?= INDEX_PAGE ?>"><button class="btn-secondary not-found-button">Terug naar Home</button></a>
+                </div>
             </div>
-        </div>
-    </section>
-</main>
+        </section>
+    </main>
 
-<?php require_once BASE_DIR . '/templates/elements/footer.php'; ?>
+    <!-- Footer -->
+    <?php require_once TEMPLATES_DIR . '/elements/footer.php'; ?>
+
 </body>
 
 </html>
