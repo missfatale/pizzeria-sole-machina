e5737a9030e39440ae8b5540eb9a96c283d35a8f
ajmschaper <ajm.schaper@student.han.nl>
2025-06-25 22:29:24 +0200

    Changed: Reconstructed Formatting

--- applicatie/public/category.php
+++ applicatie/public/category.php
@@ -1,15 +1,16 @@
 <?php
 
 // Bootstrap: Loads all Core Configurations and Path Handlers for the Project.
-require_once __DIR__ . '/../src/boostrap.php';
+require_once __DIR__ . '/../src/bootstrap.php';
 
 // Routing Logic
 require_once BASE_DIR . '/src/routing.php';
 
-// Load Whitelist
+// Load Allowlist: Load allowed categories and their subcategories.
 $allowedCategories = require BASE_DIR . '/src/config/whitelist/category-whitelist.php';
 
-// Determine Which Category to Show
+// Determine Which Category to Show based on 'category' GET parameter
+// Falls back to 404 page if the category is invalid or missing.
 $content = resolveCategoryContentFile($_GET['category'] ?? '', $allowedCategories, BASE_DIR . '/public/404.php');
 
 ?>
@@ -23,13 +24,17 @@ $content = resolveCategoryContentFile($_GET['category'] ?? '', $allowedCategorie
 
 <body>
 
-<?php require_once BASE_DIR . '/templates/elements/header.php'; ?>
+    <!-- Header + Navigation -->
+    <?php require_once BASE_DIR . '/templates/elements/header.php'; ?>
 
-<main>
-    <?php require $content; ?>
-</main>
+    <!-- Main Content -->
+    <main>
+        <?php require $content; ?>
+    </main>
 
-<?php require_once BASE_DIR . '/templates/elements/footer.php'; ?>
+    <!-- Footer -->
+    <?php require_once BASE_DIR . '/templates/elements/footer.php'; ?>
 
 </body>
+
 </html>
