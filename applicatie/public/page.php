1f9d68ad247b031bdc2888099aa063af67e0bb39
ajmschaper <ajm.schaper@student.han.nl>
2025-06-25 22:32:08 +0200

    Changed: Reconstructed Formatting

--- applicatie/public/page.php
+++ applicatie/public/page.php
@@ -1,17 +1,16 @@
 <?php
 
-/**
- * page.php
- *
- * Dynamically Renders Whitelisted pages from templates/pages/ via ?page=... parameter.
- * Uses the Whitelist to Prevent Unauthorized file access.
- */
+// Bootstrap: Loads all Core Configurations and Path Handlers for the Project.
+require_once __DIR__ . '/../src/bootstrap.php';
 
-require_once __DIR__ . '/../src/boostrap.php';
+// Routing Logic
 require_once BASE_DIR . '/src/routing.php';
 
+// Load Allowlist: Load allowed categories and their subcategories.
 $allowedPages = require BASE_DIR . '/src/config/whitelist/page-whitelist.php';
 
+// Determine Which Page to Show based on 'page' GET parameter
+// Falls back to 404 page if the page is invalid or missing.
 $page = $_GET['page'] ?? '';
 $content = resolvePageContentFile($page, $allowedPages, PUBLIC_DIR . '/404.php');
 
@@ -22,19 +21,22 @@ $content = resolvePageContentFile($page, $allowedPages, PUBLIC_DIR . '/404.php')
 
 <head>
     <?php renderDefaultHeadSection() ?>
-    <link rel="stylesheet" href="<?= BASE_URL; ?>/assets/css/privacy.css">
+    <link rel="stylesheet" href="<?= BASE_URL . '/assets/css/privacy.css' ?>">
 </head>
 
 <body>
 
-<!-- Header + Navigation -->
-<?php require_once TEMPLATES_DIR . '/elements/header.php'; ?>
+    <!-- Header + Navigation -->
+    <?php require_once TEMPLATES_DIR . '/elements/header.php'; ?>
 
-<!-- Main Content -->
-<?php require $content; ?>
+    <!-- Main Content -->
+    <main>
+        <?php require $content; ?>
+    </main>
 
-<!-- Footer -->
-<?php require_once TEMPLATES_DIR . '/elements/footer.php'; ?>
+    <!-- Footer -->
+    <?php require_once TEMPLATES_DIR . '/elements/footer.php'; ?>
 
 </body>
+
 </html>
