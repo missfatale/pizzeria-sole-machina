10447a8ce23cca05b97280bd8936267eb2c34971
ajmschaper <ajm.schaper@student.han.nl>
2025-06-26 20:41:54 +0200

    Added: add-to-cart-action.php, add-to-cart-handler.php

---
+++ applicatie/public/actions/add-to-cart-action.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * add-to-cart-action.php
+ *
+ * Public-facing entrypoint routing to add-to-cart handler.
+ */
+
+require_once __DIR__ . '/../../src/handlers/add-to-cart-handler.php';
+
+handleAddToCart();
