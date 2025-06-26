14a33298f299e87b06e1426ad630b49f4a06db9f
ajmschaper <ajm.schaper@student.han.nl>
2025-06-26 20:28:04 +0200

    Added: Front-Facing update-cart-action.php

---
+++ applicatie/public/actions/update-cart-action.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * update-cart-action.php
+ *
+ * Public-Facing Entrypoint routing to internal cart update handler.
+ */
+
+require_once __DIR__ . '/../../src/handlers/update-cart-handler.php';
+
+handleUpdateCart();
